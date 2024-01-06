c0e8f915b498f37cc11fa282204dfe5a22255832
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-01-07 00:03:22 +0530

    Add files via upload

---
+++ To clear a list.py
@@ -0,0 +1,10 @@
+##Input: [2, 3, 5, 6, 7]
+##Output: []
+##Explanation: Python list is cleared and it becomes empty so we have returned empty list.
+
+GEEK = [6, 0, 4, 1]
+print('GEEK before clear:', GEEK)
+
+# Clearing list
+GEEK.clear()
+print('GEEK after clear:', GEEK)
